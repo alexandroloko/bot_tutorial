44fe59304dc82aebcb4b7556fef5137fb5728122
alexandroloko <39981927+alexandroloko@users.noreply.github.com>
2018-07-02 13:38:47 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,8 +4,8 @@ exports.run = (client,message,args)=>{
     var usuario = message.mentions.users.first();
     if(!message.guild.member(message.author.id).hasPermissions("BAN_MEMBERS")) return message.reply("você não tem permissão de usar esse comando")
     if(message.mentions.users.size < 1) return message.reply("você não mencinou ninguém")
-    if(!message.guild.member(usuario).bannable) return message.reply("eu não posso banir essa pessoa")
-    if(razão.length < 1) return message.reply("você não colocou uma razão")
+    if(!message.guild.member(usuario).bannable) return message.reply("Você não pode banir essa pessoa")
+    if(razão.length < 1) return message.reply("Coloque um motivo!")
 
     message.guild.member(usuario).ban()
 
@@ -13,8 +13,9 @@ exports.run = (client,message,args)=>{
 
    var embed = new discord.RichEmbed()
    .setTitle("usario banido do server")
-   .addField("usuario:",usuario.username)
-   .addField("razão:", razão);
+   .addField("Usuario:",usuario.username)
+   .addField("Motivo::", razão);
+   .setColor("RANDOM")
 
    message.channel.send(embed)
 }
